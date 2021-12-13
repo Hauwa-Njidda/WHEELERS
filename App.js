6cedba64fabdd6bcab9c29acdaef0af47929c815
Hauwa <94902983+Hauwaa@users.noreply.github.com>
2021-12-13 14:59:44 +0000

    Add homepage

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
  
   return (
     <View style={styles.container}>         
-          <Login/>
+          <Home/>
     </View>
 )};
 
--- component/Home.js
+++ component/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View ,SafeAreaView, Button ,Image, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View ,SafeAreaView, Button ,Image, TextInput, TouchableOpacity,ScrollView } from 'react-native';
 
 export default function Register() {
     return (
@@ -8,9 +8,72 @@ export default function Register() {
         <Text style={styles.Text1 } >Explore and </Text>
         <Text style={styles.Text1}>Experience</Text>      
           <Text style={styles.Text2}>Discover our best experience</Text>
-        
+
+<ScrollView scrollEventThrottle={16}>
+<View style = {styles.containersicon}>
+<Text style={styles.columnText11} allowFontScaling={false}>Road</Text>
+<Text style={styles.columnText} allowFontScaling={false}>Mountain</Text>
+<Text style={styles.columnText} allowFontScaling={false}>Tandem</Text>
+<Text style={styles.columnText} allowFontScaling={false}>Cyclocross</Text>
+
+</View>
+
+<View style={styles.imageshape1}>
+<ScrollView  horizontal={true}>
+  <View >
+<Image source =
+{require('/Users/hauwakulunjidda/WHEELERS/assets/yKBtKFXCnKiaxShN9TwP3Z.jpeg')}
+style={styles.imageshape}
+/>
+  </View>
+  <View >
+<Image source =
+{require('/Users/hauwakulunjidda/WHEELERS/assets/into-the-sunset-royalty-free-image-1587501795.jpeg')}
+style={styles.imageshape}
+/>
+  </View>
+  <View >
+<Image source =
+{require('/Users/hauwakulunjidda/WHEELERS/assets/kids_bike_club.jpeg')}
+style={styles.imageshape}
+/>
+  </View>
+  
+</ScrollView>
+
+</View>
+<View style = {styles.
+containersicon}>
+<Text style={styles.columnText} allowFontScaling={false}>Join Community</Text>
+<Text style={styles.columnText1} allowFontScaling={false}>View all</Text>
+
+</View>
+<Text style={styles.columnText} allowFontScaling={false}>-----------------------------------------------------------</Text>
+</ScrollView>
+
+<ScrollView vertical={true}>
+<View style = {styles.containersicon1}>
+<Image source =
+{require('/Users/hauwakulunjidda/WHEELERS/assets/kids_bike_club.jpeg')} style={styles.imageshape2}/>
+<Text style={styles.columnText} allowFontScaling={false}>Join Community</Text>
+<Text style={styles.columnText} allowFontScaling={false}>120 Members</Text>
+<View style = {styles.containersicon11}>
+<Text style={styles.columnText1} allowFontScaling={false}>View all</Text>
+</View>
+</View>
+<View style = {styles.containersicon1}>
+<Image source =
+{require('/Users/hauwakulunjidda/WHEELERS/assets/kids_bike_club.jpeg')} style={styles.imageshape2}/>
+<Text style={styles.columnText} allowFontScaling={false}>Join Community</Text>
+<Text style={styles.columnText} allowFontScaling={false}>120 Members</Text>
+<View style = {styles.containersicon11}>
+<Text style={styles.columnText1} allowFontScaling={false}>View all</Text>
+</View>
+</View>
+</ScrollView>
 
       </SafeAreaView>
+      
  );
     }
 const styles = StyleSheet.create({
@@ -20,62 +83,98 @@ const styles = StyleSheet.create({
   alignItems: 'center',
   justifyContent: 'center',
 },
-
 Text1:{
 color: 'white',
 fontSize: 40,
 margin: 7,
+fontWeight:'500',
 },
 Text2:{
   color: 'white',
-  fontSize: 12, 
+  fontSize: 15,
+   
   },
-  Text3:{
-    color: 'white',
-    fontSize: 15, 
-    color:"yellow",
-   left: 60,
+    Text3:{
+      color: 'white',
+    fontSize: 20, 
+    fontWeight:'700',
+    paddingHorizontal: 10,
     },
-    Text33:{
-      color: 'white',
-      fontSize: 15, 
-      color:"yellow",
-      
-      },
-  
-    Text4:{
-      color: 'white',
-      fontSize: 18,
-      
-      },
 
-input:{
-width: 300,
-borderWidth: 2,
-borderRadius: 100,
-padding: 20,
-margin: 7,
-borderRadius: 100,
-backgroundColor: "white",
+  imageshape1:{
+  height:220,
+  marginTop:10,
+    },
+
+imageshape:{ 
+width:200, 
+height:220,  
+marginLeft:20,
+borderColor:"white", 
+borderWidth: 1, 
+borderRadius: 10,
 },
 
-input1:{
-color: "white",
-fontSize: 20,
+imageshape2:{
+  width:100, 
+  height:100,  
+  marginLeft:20,
+  borderWidth: 1, 
+  borderRadius: 10,
+},
+
+columnText:{
+  color:'white',
+  flex: 10,
+  alignItems: 'flex-start',
+  paddingLeft: 16,
+
+},
+columnText1:{
+  color:'blue',
+  alignItems: 'flex-start',
+  paddingLeft: 16,
+
+},
+columnText11:{
+  color:'blue',
+  flex: 10,
+  alignItems: 'flex-start',
+  paddingLeft: 16,
+
+},
+
+containersicon :{
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  alignItems :'center',
+  padding: 2,
+  margin: 7,
+  width: 390,
   },
 
-outside:{
-    
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-    alignSelf:"center",
-    height: 55,
-    width: 300,
-    borderRadius: 300,
-    backgroundColor: "blue",
-    margin: 7,
-      },  
-      
 
+  containersicon1 :{
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  alignItems :'center',
+  borderWidth : 1,
+  borderColor: 'white',
+  borderRadius: 10,
+  padding: 6,
+  margin: 8,
+  width: 340,
+  },
+
+  containersicon11 :{
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems :'center',
+    borderWidth : 1,
+    borderColor: 'white',
+    borderRadius: 10,
+    padding: 6,
+    margin: 8,
+    width:70,
+    },
 })
